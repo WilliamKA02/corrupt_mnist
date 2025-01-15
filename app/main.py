aedfa5aae4705dfc9b7811cd242f729a36d73896
unknown <s234803@dtu.dk>
2025-01-15 19:07:48 +0100

    api test

---
+++ app/main.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+
+import re
+from enum import Enum
+from http import HTTPStatus
+
+import anyio
+import cv2
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+database = {'username': [ ], 'password': [ ]}
+
+
+@app.get("/")
+def root():
+    """ Health check."""
+    response = {
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+    }
+    return response
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+class ItemEnum(Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/restric_items/{item_id}")
+def read_item(item_id: ItemEnum):
+    return {"item_id": item_id}
+
+@app.get("/query_items")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+database = {'username': [ ], 'password': [ ]}
+
+@app.post("/login/")
+def login(username: str, password: str):
+    username_db = database['username']
+    password_db = database['password']
+    if username not in username_db and password not in password_db:
+        with open('database.csv', "a") as file:
+            file.write(f"{username}, {password} \n")
+        username_db.append(username)
+        password_db.append(password)
+    return "login saved"
+
+class DomainEnum(Enum):
+    """Domain enum."""
+
+    gmail = "gmail"
+    hotmail = "hotmail"
+
+
+class Item(BaseModel):
+    """Item model."""
+
+    email: str
+    domain: DomainEnum
+
+
+@app.get("/text_model/")
+def contains_email(data: str):
+    """Simple function to check if an email is valid."""
+    regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+    return {
+        "input": data,
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+        "is_email": re.fullmatch(regex, data) is not None,
+    }
+
+@app.post("/cv_model/")
+async def cv_model(data: UploadFile = File(...), h: None | int = 28, w: None | int = 28):
+    """Simple function using open-cv to resize an image."""
+    async with await anyio.open_file("my_cat.jpg", "wb") as image:
+        content = await data.read()
+        image.write(content)
+        image.close()
+
+    img = cv2.imread("my_cat.jpg")
+    res = cv2.resize(img, (h, w))
+
+    cv2.imwrite("image_resize.jpg", res)
+
+    return {
+        "input": data,
+        "output": FileResponse("image_resize.jpg"),
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+    }
